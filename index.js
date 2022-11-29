eb00b26aaa6c0f387efc311dc214eac13a56c492
prince475 <danielmwai475@gmail.com>
2022-11-29 12:56:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+// function upperCaseCustomerName(){
+//     var ctm = customerName
+//     return ctm.toUpperCase()
+// }
+
+
+// var bestCustomer = 'not bob'
+
+// function setBestCustomer(){
+//     return bestCustomer;
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+
+var bestCustomer
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "nani"
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "yule"
+}
+
+/*
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "Kevin";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Bob";
+}
+
+
+
+
+
+
+*/
